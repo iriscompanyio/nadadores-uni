aaac9aeb0d2c83a84ac82f1a5ccc05e7b0cdee01
mbsa96 <mbsa96@gmail.com>
2024-09-12 17:42:29 -0500

    create banner home

---
+++ src/components/BannerInicio.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
+
+const slides = ["/swimming.png", "/swimming2.jpg", "/swimming3.jpg"];
+</script>
+<template>
+  <Carousel
+    v-slot="{ scrollTo, selectedSlideIndex }"
+    class="relative"
+    :plugins="[
+      Autoplay({
+        delay: 6000,
+      }),
+    ]"
+  >
+    <CarouselContent>
+      <CarouselItem v-for="slide in slides" :key="slide">
+        <div
+          class="w-full h-[560px] bg-cover bg-center"
+          :style="{ backgroundImage: `url(${slide})` }"
+        ></div>
+      </CarouselItem>
+    </CarouselContent>
+    <div
+      class="absolute bottom-4 left-1/2 -translate-x-1/2 transform flex space-x-4"
+    >
+      <button
+        v-for="(_, index) in slides"
+        :key="index"
+        class="w-[20px] h-[20px] rounded-full"
+        :class="
+          selectedSlideIndex === index ? '!bg-[#2F326E]' : 'bg-[#ffffff9c]'
+        "
+        @click="scrollTo(index)"
+      ></button>
+    </div>
+  </Carousel>
+</template>
