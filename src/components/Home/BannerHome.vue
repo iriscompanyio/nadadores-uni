212ef5b99fa82afa501951a33786fe69112bf381
dany <61592937+dany0814@users.noreply.github.com>
2025-08-05 19:20:21 -0500

    actualizaciones de banners

--- src/components/Home/BannerHome.vue
+++ src/components/Home/BannerHome.vue
@@ -7,10 +7,23 @@ import {
 import Autoplay from "embla-carousel-autoplay";
 
 const slides = [
-  "https://res.cloudinary.com/dkljgkuyy/image/upload/f_auto,q_auto/v1/nadadores/index/uqfmlcvde5pqtudzpvkh",
-  "https://res.cloudinary.com/dkljgkuyy/image/upload/f_auto,q_auto/v1/nadadores/index/oceu2ugqyfvmxru3thdx",
-  "https://res.cloudinary.com/dqpg07sfp/image/upload/v1738975638/Portada_03_dyvkue.png",
+  {
+    image: "https://ik.imagekit.io/iriscompany/nadadores-uni/banner-01.png?updatedAt=1754436610429",
+    href: "",
+  },
+  {
+    image: "https://ik.imagekit.io/iriscompany/nadadores-uni/banner-02.png?updatedAt=1754436609966",
+    href: "",
+  },
+  {
+    image: "https://ik.imagekit.io/iriscompany/nadadores-uni/banner-03.png?updatedAt=1754436610484",
+    href: "/ruta-del-nadador",
+  },
 ];
+const navigateToSlide = (href: string) => {
+  window.location.href = href;
+};
+
 </script>
 <template>
   <Carousel
@@ -26,7 +39,8 @@ const slides = [
       <CarouselItem v-for="slide in slides" :key="slide">
         <div
           class="w-full h-[560px] bg-cover bg-center"
-          :style="{ backgroundImage: `url(${slide})` }"
+          :style="{ backgroundImage: `url(${slide.image})` }"
+          @click="navigateToSlide(slide.href)"
         ></div>
       </CarouselItem>
     </CarouselContent>
